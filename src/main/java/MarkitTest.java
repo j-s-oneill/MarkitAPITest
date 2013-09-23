216ab34f4f323fd8bf6073fd882fea25acdb887a
Paul Stallworth <pstallworth@gmail.com>
2013-09-23 17:03:54 -0500

    Testing out Gson

--- src/main/java/MarkitTest.java
+++ src/main/java/MarkitTest.java
@@ -6,35 +6,35 @@
  * To change this template use File | Settings | File Templates.
  */
 
-import static spark.Spark.*;
-import spark.*;
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
+
+import static spark.Spark.get;
 
 public class MarkitTest {
 
     public static final String REST_ENDPOINT = "http://dev.markitondemand.com/Api/Quote";
-    public static final String URI_INFO_PATH = "/Quote";
     public static final String sSymbol = "CSCO";
+    public static Gson gson = new Gson();
 
     public static void main(String[] args) {
         get (new Route("/") {
             @Override
             public Object handle(Request request, Response response) {
                 response.type("text/plain");
-                return "Output:\n" + getQuote(sSymbol);
+                return getQuote(sSymbol);
             }
         });
     }
 
     public static String getQuote(String symbol) {
         String endpoint = REST_ENDPOINT + "/json?symbol=" + symbol;
+
         try {
             java.net.URL endpointURL = new java.net.URL(endpoint);
             java.net.HttpURLConnection request = (java.net.HttpURLConnection)endpointURL.openConnection();
@@ -44,10 +44,15 @@ public class MarkitTest {
             java.io.BufferedReader rd = new BufferedReader(new InputStreamReader((request.getInputStream())));
             StringBuilder response = new StringBuilder();
             String line = null;
+
+            StockQuote quote = new StockQuote();
             while ((line = rd.readLine()) != null) {
+                gson.toJson(line);
                 response.append(line);
             }
 
+            quote = gson.fromJson(response.toString(), StockQuote.class);
+            //now return the quote class and try to access its members
             return response.toString();
 
         } catch (Exception e) {
---
+++ src/main/java/StockQuote.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: stallworthpaul
+ * Date: 9/23/13
+ * Time: 4:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StockQuote {
+
+    private String status;
+    private String name;
+    private String symbol;
+    private double lastPrice;
+    private double change;
+    private double changePercent;
+    private String timestamp;
+    private double marketCap;
+    private long volume;
+    private double changeYTD;
+    private double changePercentYTD;
+    private double high;
+    private double low;
+    private double open;
+
+    StockQuote() {
+
+    }
+}
